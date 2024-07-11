b5070c8304cbe9110a2c8055e84058d72f978b4f
Rishabh Gupta <rg074523@gmail.com>
2024-07-12 01:33:32 +0530

    done

--- jindalsteel/src/components/Page1/Page1.js
+++ jindalsteel/src/components/Page1/Page1.js
@@ -1,23 +1,85 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './page1.css';
 import Jindalbg from '../../assests/jindalbg.webp'
-
-const vari = [
-  { 'var': 'Raw Gas Flow' },
-  { 'var': 'Raw Gas Temperature' },
-  { 'var': 'CO₂ In' },
-  { 'var': 'CO₂ Out' },
-  { 'var': 'Methanol Temperature' },
-]
+import axios from 'axios'
 
 const Page1 = () => {
 
+  const vari = [
+    { 'var': 'Raw Gas Flow',
+      'name':'RawGasFlow'
+     },
+    { 'var': 'Raw Gas Temperature',
+      'name':'RawGasTemperature' 
+    },
+    { 'var': 'CO₂ In' ,
+      'name':'CO2In'
+    },
+    { 'var': 'CO₂ Out' ,
+      'name':'CO2Out'
+    },
+    { 'var': 'Methanol Temperature' ,
+      'name':'MethanolTemperature'
+    },
+  ]
+
   const [inputValues, setInputValues] = useState(
-    vari.reduce((acc, item) => {
-      acc[item.var] = '';
-      return acc;
-    }, {})
+    // vari.reduce((acc, item) => {
+    //   acc[item.var] = '';
+    //   return acc;
+    // }, {})
+    {
+      'RawGasFlow':null,
+      'RawGasTemperature':null,
+      'CO2In':null,
+      'CO2Out':null,
+      'MethanolTemperature':null,
+    }
   );
+
+  const [data,setData] = useState({});
+  const [error, setError] = useState(null);
+  const [isresult, setIsResult] = useState(false);
+  const [result, setResult] = useState(null);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get('http://localhost:5000/data');
+        setData(res.data);
+        console.log(res.data); // Log the fetched data
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  useEffect(() => {
+    const checkForNullValues = (obj) => {
+      return Object.values(obj).some(value => value === null);
+    };
+    const handleCheck = () => {
+      if (!checkForNullValues(inputValues)) {
+        setShow(true);
+      }
+    };
+    handleCheck();
+  }, [inputValues]);
+
+  const Page1Calc = async () => {
+    console.log(inputValues);
+    const log_rawgasflow = await Math.log(inputValues.RawGasFlow);
+    const log_rawtemp = await Math.log(inputValues.RawGasTemperature);
+    const log_co2_diff = await Math.log(inputValues.CO2In - inputValues.CO2Out);
+    const log_co2in = await Math.log(inputValues.CO2In);
+    const log_methanoltemp = await Math.log(inputValues.MethanolTemperature);
+    const log_predicted_M = await ((data.meth.a * Math.log(data.meth.k1)) + (data.meth.b * log_rawgasflow) + (data.meth.c * log_rawtemp) + (data.meth.d * log_co2_diff) + (data.meth.e * log_co2in) + (data.meth.f * log_methanoltemp) );
+    const predicted_M = await Math.exp(log_predicted_M);
+    setResult(0.936275*predicted_M);
+    setIsResult(true);
+  };
 
   // Handle input changes
   const handleChange = (e) => {
@@ -34,27 +96,32 @@ const Page1 = () => {
       <div className='pg-1'>
         <p className='pg1-head'>Methanol Flow Calculator</p>
         <div className='pg-1-calc'>
-          {vari.map((val) => {
+          {vari.map((val,index) => {
             return (
-              <div className='pg-1-input'>
+              <div key={index} className='pg-1-input'>
                 {val.var}
                 <input
+                  key={index}
                   className='input-var'
                   type='number'
                   required
-                  name={val.var}
-                  value={inputValues[val.var]}
+                  name={val.name}
+                  value={inputValues[val.name]}
                   onChange={handleChange}
                 />
               </div>
             )
           })}
         </div>
-        <button
-          // onClick={Page2Calc()}
+        {!isresult ? 
+        (show && (<button
+          onClick={Page1Calc}
           className='cal-btn'>
           Calculate
-        </button>
+        </button>)):
+        (
+          <div className='result'>Predicted Methanol Flow: {result ? (result) : "Check your values"}</div>
+        )}
       </div>
     </>
   )
--- jindalsteel/src/components/Page2/Page2.js
+++ jindalsteel/src/components/Page2/Page2.js
@@ -1,24 +1,86 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 // import './page2.css';
 import '.././Page1/page1.css'
 import Jindalbg from '../../assests/jindalbg.webp'
-
-const vari = [
-  { 'var': 'Methanol Flow' },
-  { 'var': 'Cooling Water Inlet Temperature' },
-  { 'var': 'Cooling Water Flow' },
-  { 'var': 'Prop Out Temperature' },
-  { 'var': 'Raw Gas Flow' },
-]
+import axios from 'axios'
 
 const Page2 = () => {
 
+  const vari = [
+    { 'var': 'Methanol Flow',
+      'name':'MethanolFlow'
+     },
+    { 'var': 'Cooling Water Inlet Temperature',
+      'name':'CoolingWaterInletTemperature' 
+    },
+    { 'var': 'Cooling Water Flow' ,
+      'name':'CoolingWaterFlow'
+    },
+    { 'var': 'PropOutTemperature' ,
+      'name':'PropOutTemperature'
+    },
+    { 'var': 'Raw Gas Flow' ,
+      'name':'RawGasFlow'
+    },
+  ]
+
   const [inputValues, setInputValues] = useState(
-    vari.reduce((acc, item) => {
-      acc[item.var] = '';
-      return acc;
-    }, {})
+    // vari.reduce((acc, item) => {
+    //   acc[item.var] = '';
+    //   return acc;
+    // }, {})
+    {
+      'MethanolFlow':null,
+      'CoolingWaterInletTemperature':null,
+      'CoolingWaterFlow':null,
+      'PropOutTemperature':null,
+      'RawGasFlow':null,
+    }
   );
+
+  const [data,setData] = useState({});
+  const [error, setError] = useState(null);
+  const [isresult, setIsResult] = useState(false);
+  const [result, setResult] = useState(null);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get('http://localhost:5000/data');
+        setData(res.data);
+        console.log(res.data); // Log the fetched data
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  useEffect(() => {
+    const checkForNullValues = (obj) => {
+      return Object.values(obj).some(value => value === null);
+    };
+    const handleCheck = () => {
+      if (!checkForNullValues(inputValues)) {
+        setShow(true);
+      }
+    };
+    handleCheck();
+  }, [inputValues]);
+
+  const Page1Calc = async () => {
+    console.log(inputValues);
+    const log_methanol_flow = await Math.log(inputValues.MethanolFlow)
+    const log_cws_temp = await Math.log(inputValues.CoolingWaterInletTemperature)
+    const log_cw_flow = await Math.log(inputValues.CoolingWaterFlow)
+    const log_raw_gas_flow = await Math.log(inputValues.RawGasFlow)
+    const log_prop_out = await Math.log(inputValues.PropOutTemperature)
+    const log_predicted_dis_press = await (data.dis.a * Math.log(data.dis.k1) + data.dis.b * log_methanol_flow + data.dis.d * log_cws_temp + data.dis.e * log_cw_flow + data.dis.c * log_raw_gas_flow + data.dis.f * log_prop_out)
+    const predicted_dis_press = await Math.exp(log_predicted_dis_press)
+    setResult(predicted_dis_press);
+    setIsResult(true);
+  };
 
   // Handle input changes
   const handleChange = (e) => {
@@ -33,29 +95,34 @@ const Page2 = () => {
     <>
       <img className='bg' src={Jindalbg} />
       <div className='pg-1'>
-        <p className='pg1-head'>Discharge Pressure Calculator</p>
+        <p className='pg1-head'>Pressure Discharge Calculator</p>
         <div className='pg-1-calc'>
-          {vari.map((val) => {
+          {vari.map((val,index) => {
             return (
-              <div className='pg-1-input'>
+              <div key={index} className='pg-1-input'>
                 {val.var}
                 <input
+                  key={index}
                   className='input-var'
                   type='number'
                   required
-                  name={val.var}
-                  value={inputValues[val.var]}
+                  name={val.name}
+                  value={inputValues[val.name]}
                   onChange={handleChange}
                 />
               </div>
             )
           })}
         </div>
-        <button
-          // onClick={Page2Calc()}
+        {!isresult ? 
+        (show && (<button
+          onClick={Page1Calc}
           className='cal-btn'>
           Calculate
-        </button>
+        </button>)):
+        (
+          <div className='result'>Predicted Discharge Pressure: {result ? (result) : "Check your values"}</div>
+        )}
       </div>
     </>
   )
